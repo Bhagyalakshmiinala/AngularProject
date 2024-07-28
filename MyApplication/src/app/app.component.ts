d59b1559de80335404945f1ae2c77fbaae090f1c
Bhagyalakshmiinala <bhagyalakshmiinala24@gmail.com>
2024-07-27 21:18:45 -0400

    ngfor implementation

--- MyApplication/src/app/app.component.ts
+++ MyApplication/src/app/app.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'MyApplication';
   placeholderValue="Enter";
+  objValue="bhagyalakshmi"
 
+  onSubmit(){
+    console.log(" Enter a value",this.objValue);
+  }
+arrlist =[1,2,3,4,5,6]
+
+arrvalue = [
+  {
+    name : 'Bhagyalakshmi',
+    age : 25
+  },
+  {
+    name : 'Venkatesh',
+    age : 26
+  },
+  {
+    name : 'lakshmi',
+    age : 25
+  }
+
+]
 }
