e1eb0bc00065d12793b769d3ab8eed27e0c92592
Bhagyalakshmiinala <bhagyalakshmiinala24@gmail.com>
2024-07-28 14:15:27 -0400

    data share between comps

--- dataShare/src/app/app.component.ts
+++ dataShare/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NewcompComponent } from './newcomp/newcomp.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NewcompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dataShare';
+  title = 'App root componet ';
+  ctitle = 'App component data share with child'
 }
--- dataShare/src/app/newcomp/newcomp.component.ts
+++ dataShare/src/app/newcomp/newcomp.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-newcomp',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './newcomp.component.css'
 })
 export class NewcompComponent {
+@Input() childtitle:string
 
 }
